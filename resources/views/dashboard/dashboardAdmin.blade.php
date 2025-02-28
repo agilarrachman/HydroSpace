34da38e338a72f296deb09ca0fb77592b66cf3fa
Muhammad Fillah Alfatih <fillahalfatih@users.noreply.github.com>
2025-03-01 06:05:47 +0700

    menambahkan page daftar kustomer

--- resources/views/dashboard/dashboardAdmin.blade.php
+++ resources/views/dashboard/dashboardAdmin.blade.php
@@ -74,71 +74,12 @@
                             </a>
                         </div>
 
-                        <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+                        <div class="navbar-nav-right d-flex align-items-center justify-content-between" id="navbar-collapse">
                             <h5 class="mb-0">Daftar Admin</h5>
 
-                            <ul class="navbar-nav flex-row align-items-center ms-auto">
-
-                                <!-- User -->
-                                <li class="nav-item navbar-dropdown dropdown-user dropdown">
-                                    <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-                                        <div class="avatar avatar-online">
-                                            <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
-                                        </div>
-                                    </a>
-                                    <ul class="dropdown-menu dropdown-menu-end">
-                                        <li>
-                                            <a class="dropdown-item" href="#">
-                                                <div class="d-flex">
-                                                    <div class="flex-shrink-0 me-3">
-                                                        <div class="avatar avatar-online">
-                                                            <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
-                                                        </div>
-                                                    </div>
-                                                    <div class="flex-grow-1">
-                                                        <span class="fw-medium d-block">John Doe</span>
-                                                        <small class="text-muted">Admin</small>
-                                                    </div>
-                                                </div>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <div class="dropdown-divider"></div>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="#">
-                                                <i class="bx bx-user me-2"></i>
-                                                <span class="align-middle">My Profile</span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="#">
-                                                <i class="bx bx-cog me-2"></i>
-                                                <span class="align-middle">Settings</span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="#">
-                                                <span class="d-flex align-items-center align-middle">
-                                                    <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
-                                                    <span class="flex-grow-1 align-middle ms-1">Billing</span>
-                                                    <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
-                                                </span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <div class="dropdown-divider"></div>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="javascript:void(0);">
-                                                <i class="bx bx-power-off me-2"></i>
-                                                <span class="align-middle">Log Out</span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <!--/ User -->
-                            </ul>
+                            <div class="avatar avatar-online">
+                                <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                            </div>
                         </div>
                     </nav>
 
@@ -149,6 +90,8 @@
                         <!-- Content -->
 
                         <div class="container-xxl flex-grow-1 container-p-y">
+                            <a href="#" class="btn btn-primary mb-4">Tambah Admin</a>
+
                             <!-- Striped Rows -->
                             <div class="card">
                                 {{-- <h5 class="card-header">Striped rows</h5> --}}
@@ -231,7 +174,6 @@
                             </div>
                             <!--/ Striped Rows -->
 
-                            <button type="button" class="btn btn-primary mt-4">Tambah Admin</button>
                         </div>
                         <!-- / Content -->
 
